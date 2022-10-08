a929147776a2d1dc74568f9a97bb1d50668821b7
seraphh01 <71792603+seraphh01@users.noreply.github.com>
2022-10-08 10:37:03 +0300

    Initial Commit

---
+++ PPD-Lab1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace PPD_Lab1
+{
+    public struct Product
+    {
+        public string Name;
+        public int Price;
+
+        public Product(string name, int price)
+        {
+            Price = price;
+            Name = name;
+        }
+    }
+
+    public struct Bill
+    {
+        public Dictionary<Product, int> items;
+        public int totalPrice;
+    }
+    
+    class Program
+    {
+        private List<Bill> _sales = new List<Bill>();
+        private Dictionary<Product, int> _inventory = new ();
+        private int _totalMoney = 0;
+
+        void AddProducts()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _inventory.Add(new Product($"product{i}", i), 100);
+            }
+            
+        }
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
